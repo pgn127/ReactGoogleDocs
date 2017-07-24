3e27aa6aa2c0196a70637807407690df9e468b00
Pam Needle <pneedle@colgate.edu>
2017-07-24 12:47:38 -0700

    bold button

--- reactApp/Components/MyEditor.js
+++ reactApp/Components/MyEditor.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+
+
+const styleMap = {
+  'STRIKETHROUGH': {
+    textDecoration: 'line-through',
+},
+'BOLD': {
+    fontWeight: 'bold'
+}
+};
 
 class MyEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+    this.state = {
+        editorState: EditorState.createEmpty()
+    };
     this.onChange = (editorState) => this.setState({editorState});
   }
+
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState,
+      'BOLD'
+    ));
+  }
+
   render() {
     return (
-      <div>
-        IN EDITOR. CODE IS WORKING.
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+      <div >
+          IN EDITOR. CODE IS WORKING.
+          <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+          <div style={{border: '1px solid black'}} className="editor">
+              <Editor customStyleMap={styleMap} editorState={this.state.editorState} onChange={this.onChange} />
+          </div>
         </div>
     );
   }
