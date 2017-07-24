f47cd3d35a327ead813f049edd5bc9d66f79a078
floresfxf <fflores@colgate.edu>
2017-07-24 15:50:00 -0700

    Buttons active working

--- reactApp/Components/StyleButton.js
+++ reactApp/Components/StyleButton.js
@@ -3,33 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-    const inlineControls = {
-      fontFamily: 'Helvetica',
-      fontSize: '14px',
-      marginBottom: '5px',
-      userSelect: 'none',
-    }
-
-    const styleButton = {
-      color: '#999',
-      cursor: 'pointer',
-      marginRight: '16px',
-      paddingTop: '2px',
-      paddingLeft:  0,
-      display: 'inline-block',
-      border: '1px solid #ddd',
-      borderRadius: '5px'
-    }
-
     const activeButton = {
       color: '#5890ff',
       cursor: 'pointer',
-      marginRight: '16px',
-      paddingTop: '2px',
-      paddingLeft:  0,
-      display: 'inline-block',
       border: '1px solid #ddd',
-      borderRadius: '5px',
+      borderRadius: '3px',
   }
 
   class StyleButton extends React.Component {
@@ -44,11 +22,11 @@ import ReactDOM from 'react-dom';
         render() {
           let className = this.props.label;
           if (this.props.active) {
-            className = activeButton;
+            className += activeButton;
           }
 //style={className}
           return (
-            <button className={this.props.label} onMouseDown={this.onToggle}>
+            <button className={this.props.label} style={this.props.active ? activeButton: {}} onMouseDown={this.onToggle}>
               {/* {this.props.label} */}
             </button>
           );
