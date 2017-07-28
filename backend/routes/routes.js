ef9ac6316da7c30e1d8997c9945707bc45a7dca1
Pam Needle <pneedle@colgate.edu>
2017-07-28 13:39:48 -0700

    fixing error messages

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -210,7 +210,7 @@ router.post('/documents/add/collaborator/:documentId', function(req, res){
 
                 } else {
                     console.log('error no users found in update collabs users.length==0');
-                    throw new Error('One or more of those emails were not valid'+err)
+                    throw new Error('One or more of those emails were not valid')
                 }
             })
             .catch(err => {
